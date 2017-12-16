3eeb670f52a28c444f1dfb468a0607902eb45767
heiuchi23 <lotteyoushowfranco4@gmail.com>
2017-12-17 03:40:51 +0900

    Add weekly and monthly summarising. Published as vesrion 0.02.

--- data_cleansing.R
+++ data_cleansing.R
@@ -4,7 +4,8 @@ library(purrr)
 library(purrrlyr)
 library(stringr)
 
-blocks <- fread("./data/test_data.csv") %>% as.data.frame()
+blocks <- fread("./data/1215.csv") %>% as.data.frame() %>% 
+  dplyr::select(TIMESTAMP, TOTALFEE)
 
 blocks$TIMESTAMP <- as.numeric(blocks$TIMESTAMP)
 blocks$TOTALFEE <- as.numeric(blocks$TOTALFEE) / 1000000
@@ -13,30 +14,53 @@ blocks$GMT_TIME <- as.POSIXct("2015-03-29 00:06:25", "GMT") + blocks$TIMESTAMP
 blocks$YYMM <- year(blocks$GMT_TIME) * 100 + month(blocks$GMT_TIME)
 blocks$YYMM_Week <- year(blocks$GMT_TIME) * 100 + week(blocks$GMT_TIME)
 blocks$Date <- as.Date(blocks$GMT_TIME)
-#blocks$YYMMDD <- blocks$YYMM * 100 + mday(blocks$GMT_TIME)
 
 monthly <- blocks %>% dplyr::group_by(YYMM) %>% 
   purrrlyr::by_slice(~ mean(.x$TOTALFEE)) %>% 
-  dplyr::rename(Fee = .out)
-monthly$Fee <- as.numeric(monthly$Fee)
+  dplyr::rename(Mean = .out)
 monthly$Date <- seq(as.Date("2015-03-29"), length.out = nrow(monthly), by = "1 month")
 
-monthly_p <- plot_ly(x = ~monthly$Date, y = ~monthly$Fee, mode = 'lines', type = 'scatter')
+monthly_m <- blocks %>% dplyr::group_by(YYMM) %>% 
+  purrrlyr::by_slice(~ max(.x$TOTALFEE)) %>% 
+  dplyr::rename(Max = .out)
+monthly_m$Max <- as.numeric(monthly_m$Max)
 
+monthly_null <- blocks %>% dplyr::group_by(YYMM) %>% 
+  purrrlyr::by_slice(function(x){
+    return(x %>% dplyr::filter(TOTALFEE == 0) %>% nrow() / nrow(x))
+  })
+
+monthly$Mean <- as.numeric(monthly$Mean)
+monthly$Max <- as.numeric(monthly_m$Max)
+monthly$Null <- as.numeric(monthly_null$.out)
+
+# Weekly --------------
 
 weekly <- blocks %>% dplyr::group_by(YYMM_Week) %>% 
   purrrlyr::by_slice(~ mean(.x$TOTALFEE)) %>% 
-  dplyr::rename(Fee = .out)
-weekly$Fee <- as.numeric(weekly$Fee)
+  dplyr::rename(Mean = .out)
+weekly$Fee <- as.numeric(weekly$Mean)
 weekly$Date <- seq(as.Date("2015-03-29"), length.out = nrow(weekly), by = "1 week")
 
-weekly_p <- plot_ly(x = ~weekly$Date, y = ~weekly$Fee, mode = 'lines', type = 'scatter')
+weekly_m <- blocks %>% dplyr::group_by(YYMM_Week) %>% 
+  purrrlyr::by_slice(~ max(.x$TOTALFEE)) %>% 
+  dplyr::rename(Max = .out)
+weekly_m$Max <- as.numeric(weekly_m$Max)
 
+weekly_null <- blocks %>% dplyr::group_by(YYMM_Week) %>% 
+  purrrlyr::by_slice(function(x){
+    return(x %>% dplyr::filter(TOTALFEE == 0) %>% nrow() / nrow(x))
+  })
+
+weekly$Mean <- as.numeric(weekly$Mean)
+weekly$Max <- as.numeric(weekly_m$Max)
+weekly$Null <- as.numeric(weekly_null$.out)
+
+# Daily --------------
 
 daily <- blocks %>% dplyr::group_by(Date) %>% 
   purrrlyr::by_slice(~ mean(.x$TOTALFEE)) %>% 
   dplyr::rename(Mean = .out)
-daily$Mean <- as.numeric(daily$Mean)
 
 daily_m <- blocks %>% dplyr::group_by(Date) %>% 
   purrrlyr::by_slice(~ max(.x$TOTALFEE)) %>% 
@@ -48,6 +72,11 @@ daily_null <- blocks %>% dplyr::group_by(Date) %>%
     return(x %>% dplyr::filter(TOTALFEE == 0) %>% nrow() / nrow(x))
   })
 
-daily_p <- plot_ly(x = ~daily$Date, y = ~daily$Mean, type = 'bar')
+daily$Mean <- as.numeric(daily$Mean)
+daily$Max <- as.numeric(daily_m$Max)
+daily$Null <- as.numeric(daily_null$.out)
+
+daily_p <- plot_ly(x = ~daily$Date, y = ~daily$Mean, type = 'bar') %>% 
+  layout(xaxis = list(title = "aaaaa"))
 daily_p <- plot_ly(x = ~daily$Date, y = ~daily$Max, mode = 'lines', type = 'bar')
 daily_p <- plot_ly(x = ~daily_null$Date, y = ~daily_null$.out, mode ="lines", type = "scatter")
--- server.R
+++ server.R
@@ -13,9 +13,9 @@ library(plotly)
 library(stringr)
 library(zoo)
 
-daily <- readRDS("./daily2")
-#weekly <- readRDS("./weekly")
-#monthly <- readRDS("./monthly")
+daily <- readRDS("./daily171215")
+weekly <- readRDS("./weekly171215")
+monthly <- readRDS("./monthly171215")
 
 shinyServer(function(input, output) {
   
@@ -47,6 +47,7 @@ shinyServer(function(input, output) {
     output$daily_plot <- renderPlotly({
       plot_ly(x = ~tmp_data$Date, y = ~tmp_data$Y, type = 'bar') %>% 
         layout(
+          xaxis = list(title = 'Date'),
           yaxis = list(title = Ylab)
         )
     })
@@ -56,72 +57,11 @@ shinyServer(function(input, output) {
   
   # Default
   output$daily_plot <- renderPlotly({
-    plot_ly(x = ~daily$Date, y = ~daily$Mean, type = 'bar')
+    plot_ly(x = ~daily$Date, y = ~daily$Mean, type = 'bar') %>% 
+      layout(
+        xaxis = list(title = 'Date'),
+        yaxis = list(title = 'Average')
+      )
   })
-  
-  # observeEvent(input$reload,{
-  #   begin <- as.POSIXct(as.Date(input$daterange[1]))
-  #   end <- as.POSIXct(as.Date(input$daterange[2]))
-  #   
-  #   tmp_blocks <- blocks %>% dplyr::filter(between(GMT_TIME, begin, end))
-  #   
-  #   
-  #   interval <- switch(input$moving,
-  #                      "1 hour" = 60,
-  #                      "1 day" = 1440,
-  #                      "3 days" = 1440 * 3,
-  #                      "7 days" = 1440 * 7,
-  #                      "14 days" = 1440 * 14,
-  #                      "28 days" = 1440 * 28,
-  #                      60
-  #                      )
-  #   
-  #   # Summarise
-  #   average_fee <- mean(tmp_blocks$TOTALFEE)
-  #   median_fee  <- median(tmp_blocks$TOTALFEE)
-  #   max_fee     <- max(tmp_blocks$TOTALFEE)
-  #   null_percent <- tmp_blocks %>% dplyr::filter(TOTALFEE == 0) %>% nrow() / nrow(tmp_blocks)
-  #   
-  #   summary_table <- data.frame(
-  #     AVERAGE = average_fee,
-  #     MEDIAN  = median_fee,
-  #     MAX = max_fee,
-  #     NULLBLOCK = str_c(str_sub(as.character(null_percent * 100), 1,2), "%")
-  #   )
-  #   
-  #   output$summary_table <- renderTable({summary_table}, width = 170, align = "c")
-  #   
-  #   # Generate Plot
-  #   tmp_blocks$smoothed <- c(rep(average_fee, interval - 1),
-  #                            rollmean(tmp_blocks$TOTALFEE, interval))
-  #   
-  #   output$HarvestPlot <- renderPlot({
-  #     ggplot() +
-  #       theme_bw() +
-  #       xlab("Time") + 
-  #       ylab("HARVESTED FEE") +
-  #       layer(data = tmp_blocks,
-  #             mapping = aes(x = GMT_TIME, y = smoothed),
-  #             geom = "line",
-  #             params = list(color = "red"),
-  #             stat = "identity",
-  #             position = "identity"
-  #             )
-  #   })
-  #   
-  #     
-  #   output$rawdataPlot <- renderPlot({
-  #     ggplot() +
-  #       theme_bw() +
-  #       xlab("Time") + 
-  #       ylab("HARVESTED FEE") +
-  #       coord_cartesian(ylim = 0:1000) +
-  #       layer(data = tmp_blocks, 
-  #             mapping = aes(x = GMT_TIME, TOTALFEE),
-  #             geom = "line",
-  #             stat = "identity",
-  #             position = "identity")
-  #   })
-  # })
 
 })
